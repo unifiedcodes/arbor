1c6ce996f9e86d78bf2a434939a0f6ebdfb06268
Harshit Mishra <66333228+harshittmishra@users.noreply.github.com>
2025-09-01 18:12:21 +0530

    ORM supports FCRUD and Relations with Lazy Loading.

--- src/database/orm/ModelQuery.php
+++ src/database/orm/ModelQuery.php
@@ -11,31 +11,27 @@ class ModelQuery
 {
     protected QueryBuilder $builder;
     protected Database $database;
-    protected string $modelClass;
+    protected string $model;
 
 
-    public function __construct(Database $database, string $modelClass)
+    public function __construct(Database $database, string $model)
     {
         $this->database = $database;
-        $this->modelClass = $modelClass;
-        $this->builder = $database->table($modelClass::getTableName());
+        $this->model = $model;
+        $this->spawnBuilder();
     }
 
 
-    public function create(array $attributes)
+    protected function spawnBuilder(): QueryBuilder
     {
-        // insert into table, get back auto-id
-        $id = $this->builder->insert($attributes);
-
-        $attributes = array_merge($attributes, ['id' => $id]);
-
-        return $this->hydrate($attributes);
+        $this->builder = $this->database->table($this->model::getTableName());
+        return $this->builder;
     }
 
 
     protected function hydrate(array $attributes)
     {
-        $class = $this->modelClass;
+        $class = $this->model;
         return new $class($attributes, true);
     }
 
@@ -50,5 +46,60 @@ class ModelQuery
 
         // If the builder returns itself (for chaining), we return ModelQuery
         return $result === $this->builder ? $this : $result;
+    }
+
+
+    public function create(array $attributes)
+    {
+        // insert into table, get back inserted-id
+        $id = $this->builder->insert($attributes);
+
+        $attributes = array_merge($attributes, ['id' => $id]);
+
+        return $this->hydrate($attributes);
+    }
+
+
+    public function find(mixed $id, array $columns = ['*'])
+    {
+        // Get table + primary key from the model
+        $primaryKey = $this->model::getPrimaryKey();
+
+        // Run the query on QueryBuilder
+        $record = $this->builder->select($columns)
+            ->where($primaryKey, $id)
+            ->first();
+
+        // Hydrate into Model instance
+        return $record
+            ? $this->hydrate($record)
+            : null;
+    }
+
+
+    public function get(?array $columns = null): array
+    {
+        // Run builder get
+        $records = $this->builder->get($columns);
+
+        // Hydrate each record into a model
+        $models = [];
+        foreach ($records as $record) {
+            $models[] = $this->hydrate($record);
+        }
+
+        return $models;
+    }
+
+
+    public function first(?array $columns = null)
+    {
+        // Run builder first
+        $record = $this->builder->select($columns)->first();
+
+        // Hydrate into a model if found
+        return $record
+            ? $this->hydrate($record)
+            : null;
     }
 }

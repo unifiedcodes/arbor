674f140131c48d5807326c7da01469a7b4e4bea0
Harshit Mishra <66333228+harshittmishra@users.noreply.github.com>
2025-09-04 22:49:33 +0530

    ORM : eager loading added for BelongsTo relationship.

--- src/database/orm/relations/BelongsTo.php
+++ src/database/orm/relations/BelongsTo.php
@@ -8,20 +8,71 @@ use Arbor\database\orm\Model;
 
 class BelongsTo extends Relationship
 {
+
+    protected $ownerKey;
+    protected $foreignKey;
+    protected $foreignValue;
+    protected $relatedInstance;
+
+
     public function __construct(Model $parent, string $related, string $foreignKey, ?string $ownerKey = null)
     {
-        $relatedInstance = new $related;
-        $ownerKey = $ownerKey ?? $relatedInstance->getPrimaryKey();
-        $foreignValue = $parent->getAttribute($foreignKey);
+        $this->relatedInstance = new $related;
+        $this->ownerKey = $ownerKey ?? $this->relatedInstance->getPrimaryKey();
+        $this->foreignKey = $foreignKey;
+        $this->foreignValue = $parent->getAttribute($this->foreignKey);
 
         // Build the query
-        $query = $related::query()->where($ownerKey, $foreignValue);
+        $query = $related::query()->where($this->ownerKey, $this->foreignValue);
 
         parent::__construct($parent, $query);
     }
 
+
     public function resolve()
     {
         return $this->query->first();
+    }
+
+
+    public function eagerLoad(string $relationName, array $models)
+    {
+        if (empty($models)) {
+            return [];
+        }
+
+        $keys = $this->getModelKeys($models, $this->foreignKey);
+
+        if (empty($keys)) {
+            return [];
+        }
+
+        // Step 2: Query all parents where ownerKey IN (...)
+        $related = $this->relatedInstance::query()->whereIn($this->ownerKey, $keys)->get();
+
+        return $this->match($relationName, $models, $related);
+    }
+
+
+    public function match(string $relationName, array $models, array $related): array
+    {
+        // Build dictionary: ownerKey => related model
+        $dictionary = [];
+        foreach ($related as $rel) {
+            $dictionary[$rel->getAttribute($this->ownerKey)] = $rel;
+        }
+
+        // Attach to each child model
+        foreach ($models as $model) {
+            $foreignValue = $model->getAttribute($this->foreignKey);
+            $owner = $dictionary[$foreignValue] ?? null;
+
+            $model->setRelation(
+                $relationName,
+                $owner
+            );
+        }
+
+        return $models;
     }
 }

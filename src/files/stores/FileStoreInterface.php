9455643f4613e96491d5d268273da772d3f21903
Harshit Mishra <66333228+harshittmishra@users.noreply.github.com>
2026-02-08 18:24:42 +0530

    Stores, Filters, Transformers added in Files Module

---
+++ src/files/stores/FileStoreInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Arbor\files\stores;
+
+
+use Arbor\files\FileContext;
+
+
+interface FileStoreInterface
+{
+    public function write(FileContext $context, string $path): void;
+
+    public function read(string $path): mixed;
+
+    public function exists(string $path): bool;
+
+    public function url(string $path): string;
+
+    public function delete(string $path): void;
+
+    public function key(): string;
+}
